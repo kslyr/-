0c52ffe27e1122f7c7d3fcf7b11ff28acc5df7f8
kslyr <st9252076@stud.duikt.edu.ua>
2026-03-26 10:52:20 +0200

    Module1(1)
    1 Завдання

---
+++ Module1/Module1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace Module1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            string inputFile = "textPD24.txt";
+            string outputFile = "resultPD24.txt";
+
+            string initialtext = "Ціни на DDR4 злетіли майже в 9 разів за рік.\n" + "Мирон Маркевич обрав лідера збірної України перед матчем зі Швецією.\n" + "Топ-10 продуктів з найпотужнішими антиоксидантами.\n";
+
+            File.WriteAllText(inputFile, initialtext);
+
+            File.WriteAllText(outputFile, "Результат:");
+            Console.WriteLine("Обробка...");
+
+            TextProcess.ProcessFile(inputFile, outputFile, TextProcess.ToUpperCase, "UPERCASE");
+            TextProcess.ProcessFile(inputFile, outputFile, TextProcess.CountSymbols, "К-сть символів");
+            TextProcess.ProcessFile(inputFile, outputFile, TextProcess.CountWords, "К-сть слів");
+
+            Console.WriteLine("Обробка завершена.\n");
+        }
+    }
+}
---
+++ Module1/Module1/TextProcess.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Module1
+{
+    public class TextProcess
+    {
+        public delegate string TextOper(string text);
+
+        public static string ToUpperCase(string text)
+        {
+            return text.ToUpper();
+        }
+        public static string CountSymbols(string text)
+        {
+            return $"К-сть символів: {text.Length}";
+        }
+        public static string CountWords(string text)
+        {
+            char[] separators = { ' ', '\t', '.', ',', '!', '?', '-', ';' };
+            string[] words = text.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+            return $"Кількість слів: {words.Length}";
+        }
+        public static void ProcessFile(string inputFile, string outputFile, TextOper oper, string operName)
+        {
+            using (StreamReader reader = new StreamReader(inputFile))
+            using(StreamWriter writer = new StreamWriter(outputFile, true))
+            {
+                writer.WriteLine($"Робота: {operName}");
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    string result = oper(line);
+
+                    writer.WriteLine(result);
+
+                }
+
+            }
+        }
+
+    }
+}
